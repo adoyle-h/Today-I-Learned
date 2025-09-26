bccb48eb888da103ab216ac0dae95ec26ee6ffbc
ADoyle <adoyle.h@gmail.com>
2025-09-26 14:15:49 +0800

    chore: add .obsidian folder

---
+++ .obsidian/plugins/obsidian-market-proxy-2.0.1/README.md
@@ -0,0 +1,15 @@
+# 插件主题市场代理插件 For Obsidian
+
+为 Obsidian 插件市场和 主题市场 提供网络代理功能，
+旨在解决中国大陆用户插件市场和主题市场无法使用的问题,支持PC和移动端。
+
+### 上手指南
+
+1. 下载压缩包
+2. 解压 obsidian-market-proxy.zip
+3. 将解压的文件夹放入笔记目录下的插件目录内。如：XXX/.obsidian/plugins
+4. 在 Obsidian 的设置中启用插件
+
+
+
+
---
+++ .obsidian/plugins/obsidian-market-proxy-2.0.1/src/main.ts
@@ -0,0 +1,84 @@
+import { Plugin, Platform } from "obsidian"
+
+class AppProxy {
+  public sendHandle: any
+
+  constructor() {
+    ; (window as any).sendHandle = (window as any).electron.ipcRenderer.send
+  }
+
+  register() {
+    ; (window as any).electron.ipcRenderer.send = function(a: any, b: any, e: any, ...rest: any) {
+      UrlRewrite(e)
+        ; (window as any).sendHandle(a, b, e, ...rest)
+    }
+  }
+
+  unRegister() {
+    ; (window as any).electron.ipcRenderer.send = (window as any).sendHandle
+  }
+}
+
+class NativeProxy {
+  constructor() {
+    ; (window as any).sendHandle = (window as any).Capacitor.toNative
+  }
+  register() {
+    ; (window as any).toNativeContainer = function() {
+      ; (window as any).Capacitor.toNative = (a: any, b: any, c: any, d: any) => {
+        if (a == "App" && b == "requestUrl") {
+          UrlRewrite(c)
+        }
+        return (window as any).sendHandle(a, b, c, d)
+      }
+    }
+      ; (window as any).toNativeContainer()
+  }
+
+  unRegister() {
+    ; (window as any).Capacitor.toNative = (window as any).sendHandle
+  }
+}
+
+function isNeedUrlRewrite(e: any): boolean {
+  if (!e || !e.url) {
+    return false
+  }
+  if (e.url.startsWith("https://github.com/") || e.url.startsWith("https://raw.githubusercontent.com/")) {
+    return true
+  }
+  return false
+}
+
+function UrlRewrite(e: any): void {
+  if (e && e.url && isNeedUrlRewrite(e)) {
+    const src = e.url
+    e.url = "https://ghfast.top/" + e.url
+
+    if ((window as any).Capacitor.isLoggingEnabled) {
+      window.console.info(JSON.stringify({ src: src, proxy: e.url }))
+    }
+    if (!e.headers) {
+      e.headers = {}
+    }
+    e.headers["content-type"] = "application/x-www-form-urlencoded"
+    e.headers["Access-Control-Allow-Origin"] = "*"
+  }
+}
+
+export default class MarketProxy extends Plugin {
+  proxy: AppProxy | NativeProxy
+
+  async onload() {
+    if (Platform.isDesktop) {
+      this.proxy = new AppProxy()
+    } else {
+      this.proxy = new NativeProxy()
+    }
+    this.proxy.register()
+  }
+
+  onunload() {
+    this.proxy.unRegister()
+  }
+}
